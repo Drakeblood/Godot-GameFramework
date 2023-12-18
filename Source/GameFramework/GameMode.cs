f493e4e76226b13a73daddec6070371cbe149936
Drakeblood <56769943+Drakeblood@users.noreply.github.com>
2023-12-18 20:08:49 +0100

    Update Level.cs

--- Source/GameFramework/GameMode.cs
+++ Source/GameFramework/GameMode.cs
@@ -1,17 +1,23 @@
+using GameFramework.Statics;
 using Godot;
 
 namespace GameFramework.System
 {
     public partial class GameMode : Node
     {
+        private GFSceneTree gfSceneTree;
+
         public virtual void InitGame(GFSceneTree sceneTree)
         {
-
+            gfSceneTree = sceneTree;
         }
 
-        //public virtual void CreatePlayer(List<>)
+        //public virtual void CreatePlayer()
         //{
+            //if(gfSceneTree.CurrentLevel.PlayerStartsLocations.Count > 0)
+            //{
 
+            //}
         //}
     }
 }
--- Source/GameFramework/Level.cs
+++ Source/GameFramework/Level.cs
@@ -10,7 +10,8 @@ namespace GameFramework.System
 
         public virtual void InitLevel(GFSceneTree sceneTree)
         {
-            playerStartsLocations.Resize(playerStartsNodePathes.Count);
+            PlayerStartsLocations.Resize(playerStartsNodePathes.Count);
+            PlayerStartsRotations.Resize(playerStartsNodePathes.Count);
 
             for (int i = 0; i < playerStartsNodePathes.Count; i++)
             {
@@ -18,16 +19,19 @@ namespace GameFramework.System
 
                 if (node is Node2D playerStart2D)
                 {
-                    playerStartsLocations[i] = new Vector3(playerStart2D.Position.X, playerStart2D.Position.Y, 0f);
+                    PlayerStartsLocations[i] = new Vector3(playerStart2D.Position.X, playerStart2D.Position.Y, 0f);
+                    PlayerStartsRotations[i] = new Vector3(playerStart2D.Rotation, 0f, 0f);
                 }
                 else if (node is Node3D playerStart3D)
                 {
-                    playerStartsLocations[i] = playerStart3D.Position;
+                    PlayerStartsLocations[i] = playerStart3D.Position;
+                    PlayerStartsRotations[i] = playerStart3D.Rotation;
                 }
             }
         }
 
-        private Array<Vector3> playerStartsLocations = new Array<Vector3>();
+        public Array<Vector3> PlayerStartsLocations = new Array<Vector3>();
+        public Array<Vector3> PlayerStartsRotations = new Array<Vector3>();
 
         [Export]
         private Array<NodePath> playerStartsNodePathes = new Array<NodePath>();
